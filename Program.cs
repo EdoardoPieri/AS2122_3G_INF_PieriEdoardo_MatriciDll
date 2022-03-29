e8872d619aedf4e64f9a4913c3820c19e2c167e4
PC07 <PC07@INFO6-5470>
2022-03-29 10:49:30 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AS2122_3G_INF_PieriEdoardo_MatriciDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
