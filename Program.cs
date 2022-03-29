555e022252956947beafc8f6624314bb9e172cfc
EdoardoPieri <edoardopieri05@libero.it>
2022-03-29 11:20:37 +0200

    creazione progetto dll

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Matrici;
 
 namespace AS2122_3G_INF_PieriEdoardo_MatriciDll
 {
@@ -6,7 +7,42 @@ namespace AS2122_3G_INF_PieriEdoardo_MatriciDll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Matrici matr = new Matrici();
+            Console.WriteLine("Inserisci la dimensione della matrice quadrata");
+            int matSize = Convert.ToInt32(Console.ReadLine());
+
+            // definizione e istanziazione matrice in base alle dimensioni indicate dall'utente
+            int[,] mat = new int[matSize, matSize];
+
+            // carica la matrice quadrata mat di una dimensione generica con numeri casuali come indicati 
+            matr.CaricaMatrice(ref matr);
+
+            // visualizza la matrice
+            matr.StampaMatrice(matr);
+
+            // conta valori nella matrice
+            Console.WriteLine("Inserire il numero per cui desidera cercare il numero di occorrenze");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int nValori = matr.ContaValore(matr, numero);
+            Console.WriteLine($"Dentro la matrice ci sono {nValori} elementi con valore {numero}");
+
+            int min = 0, max = 0;
+            double medio = 0;
+
+            // calcola il valore medio (ritornato) e il min e max della matrice
+            medio = matr.CalcolaMinMaxMedio(matr, ref min, ref max);
+            Console.WriteLine("Il valore massimo è " + max);
+            Console.WriteLine("Il valore minimo è " + min);
+            Console.WriteLine("Il valore medio della matrice è " + medio);
+
+            // inverte i valori della prima colonna della matrice con l'ultima
+            matr.InvertiColonne(ref matr);
+            // visualizza la matrice
+            matr.StampaMatrice(matr);
+
+            // stampa la cornice della matrice in senso orario a partire da [0,0]
+            matr.StampaCornice(matr);
         }
     }
 }
